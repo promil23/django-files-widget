41c992465462c63baa85536b7f76d49c8925f958
Łukasz Banasiak <lukasz@banasiak.me>
2014-08-12 20:55:34 +0200

    remove unnecessary models, forms, fields

--- topnotchdev/files_widget/fields.py
+++ topnotchdev/files_widget/fields.py
@@ -1,16 +1,10 @@
-import os
-
 from django.db import models
 from django import forms
-from django.core import exceptions, validators
 from django.utils.translation import ugettext_lazy as _
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from forms import FilesFormField, BaseFilesWidget, FileWidget, FilesWidget, ImageWidget, ImagesWidget
-from files import manage_files_on_disk
+from forms import FilesFormField, BaseFilesWidget, FilesWidget, ImagesWidget
 import controllers
-from conf import *
+from settings import *
 
 
 def formfield_defaults(self, default_widget=None, widget=None, form_class=FilesFormField, required=True, **kwargs):
@@ -26,6 +20,7 @@ def formfield_defaults(self, default_widget=None, widget=None, form_class=FilesF
 
     return defaults
 
+
 def save_all_data(self, instance, data):
     # Save old data to know which images are deleted.
     # We don't know yet if the form will really be saved.
@@ -35,34 +30,12 @@ def save_all_data(self, instance, data):
     setattr(instance, MOVED_VALUE_STR % self.name, data.moved_files)
 
 
-class FileField(models.CharField):
-    description = _("File")
-    attr_class = controllers.FilePath
-
-    def __init__(self, max_length=200, **kwargs):
-        super(FileField, self).__init__(max_length=max_length, **kwargs)
-
-    def contribute_to_class(self, cls, name):
-        super(FileField, self).contribute_to_class(cls, name)
-        receiver(post_save, sender=cls)(manage_files_on_disk)
-        setattr(cls, self.name, controllers.FilesDescriptor(self))
-
-    def save_form_data(self, instance, data):
-        save_all_data(self, instance, data)
-        super(FileField, self).save_form_data(instance, data)
-
-    def formfield(self, default_widget=FileWidget(), **kwargs):
-        defaults = formfield_defaults(self, default_widget, **kwargs)
-        return super(FileField, self).formfield(**defaults)
-
-
 class FilesField(models.TextField):
     description = _("Files")
     attr_class = controllers.FilePaths
 
     def contribute_to_class(self, cls, name):
         super(FilesField, self).contribute_to_class(cls, name)
-        receiver(post_save, sender=cls)(manage_files_on_disk)
         setattr(cls, self.name, controllers.FilesDescriptor(self))
 
     def save_form_data(self, instance, data):
@@ -74,15 +47,6 @@ class FilesField(models.TextField):
         return super(FilesField, self).formfield(**defaults)
 
 
-class ImageField(FileField):
-    description = _("Image")
-    attr_class = controllers.ImagePath
-
-    def formfield(self, default_widget=ImageWidget(), **kwargs):
-        defaults = formfield_defaults(self, default_widget, **kwargs)
-        return super(ImageField, self).formfield(**defaults)
-
-
 class ImagesField(FilesField):
     description = _("Images")
     attr_class = controllers.ImagePaths
@@ -90,13 +54,3 @@ class ImagesField(FilesField):
     def formfield(self, default_widget=ImagesWidget(), **kwargs):
         defaults = formfield_defaults(self, default_widget, **kwargs)
         return super(ImagesField, self).formfield(**defaults)
-
-
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^topnotchdev\.files_widget\.fields\.FileField"])
-    add_introspection_rules([], ["^topnotchdev\.files_widget\.fields\.FilesField"])
-    add_introspection_rules([], ["^topnotchdev\.files_widget\.fields\.ImageField"])
-    add_introspection_rules([], ["^topnotchdev\.files_widget\.fields\.ImagesField"])
-except ImportError:
-    pass
